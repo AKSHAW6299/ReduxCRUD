1a6c2fd2db55c714ff62eec8c50438d3fd192097
Anand6262 <pyexpert62@gmail.com>
2025-03-24 12:54:58 +0530

    .

---
+++ src/actions/index.js
@@ -0,0 +1,28 @@
+// CREATE
+export const createData = () => {
+    return {
+        type: 'CREATE',
+
+    }
+}
+
+// UPDATE
+export const updateData = () => {
+    return {
+        type: 'UPDATE'
+    }
+}
+
+// DELETE
+export const deleteData = () => {
+    return {
+        type: 'DELETE'
+    }
+}
+
+// RESET
+export const resetData = () => {
+    return {
+        type: 'RESET'
+    }
+}
