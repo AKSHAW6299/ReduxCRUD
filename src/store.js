97b7508e11b5792b17e6dc8a632ee88418c5dc05
Anand6262 <pyexpert62@gmail.com>
2025-03-24 15:07:37 +0530

    chanegs and bug

--- src/store.js
+++ src/store.js
@@ -3,6 +3,6 @@ import rootReducer from './reducer/index'
 
 
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store;
